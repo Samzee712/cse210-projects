34e3a94cae14478ea284fd7d8802d0bab9beb645
unknown <samiwizee@gmail.com>
2024-06-10 11:43:15 +0100

    I have finished with the prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+
+        bool play = true;
+        while (play)
+        {
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessCount = 0;
+        
+        bool correctGuessed = false;
+        while (!correctGuessed)
+
+        {
+            Console.Write("What is your Guess? ");
+            string input = Console.ReadLine();
+
+            int guess;
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("Invalid Input. Please enter a valid number. ");
+                continue;
+            }
+            guessCount++;
+
+            if (guess < magicNumber) 
+            {
+            Console.WriteLine("Higher");
+
+            }
+
+            else if (guess > magicNumber)
+            {
+            Console.WriteLine("Lower ");
+
+            }
+            else
+            {
+            Console.WriteLine("Congratulations! You guess it. ");
+            correctGuessed = true;
+
+            }
+        }
+
+        //Display the number of guessed count.
+        Console.WriteLine($"Your guessed number is: {guessCount} guesses ");
+
+        //Ask if the user wants to play again.
+        Console.Write("Do you want to play again? ");
+        string response = Console.ReadLine().ToLower();
+        if (response != "yes")
+        {
+            play = false;
+        }
+        
+    }
+    // End the Game.
+    Console.WriteLine("Thank you for playing! ");
+
     }
 }
