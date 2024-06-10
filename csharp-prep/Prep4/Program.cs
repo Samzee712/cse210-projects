24499137d7b0e198aca7c69602483969cf3f5698
unknown <samiwizee@gmail.com>
2024-06-10 12:26:16 +0100

    I have completed the prep 4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a lst of numbers, type 0 when finished. ");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+
+            }
+          
+         } while (number != 0);
+
+          int sum = numbers.Sum();
+          double average = numbers.Average();
+          int max = numbers.Max();
+
+          Console.WriteLine($"The sum is: {sum} ");
+          Console.WriteLine($"The average is: {average} ");
+          Console.WriteLine($"The Largest number in the list is: {max} ");
+
+          //To find the smallest number in the list
+          int smallestPositive = numbers.Where(n => n > 0).DefaultIfEmpty().Min();
+          Console.WriteLine($"The smallest postive number in the list is: {smallestPositive} ");
+
+          numbers.Sort();
+          Console.WriteLine("The sorted list is: ");
+          foreach (int num in numbers)
+          {
+            Console.WriteLine(num);
+          }
+
+
+        
     }
 }
